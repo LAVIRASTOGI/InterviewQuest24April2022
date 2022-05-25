e1c661af8d18a20ae189b70ac03a3b59a53177b7
Lavi Rastogi <Lavi.Rastogi@ibm.com>
2022-05-25 16:09:58 +0530

    ployfills call apply bind

--- creatingevents/polyfills/server.js
+++ creatingevents/polyfills/server.js
@@ -1,4 +1,4 @@
-let inputArr=[1,2,4,5,6,7,8];
+let inputArr = [1, 2, 4, 5, 6, 7, 8];
 // // let outputarr=inputArr.filter((ele)=>ele%2==0);
 // // console.log(outputarr)
 // // output --[ 2, 4, 6, 8 ]
@@ -16,7 +16,7 @@ Array.prototype.newFilter = function (fn) {
 console.log(inputArr.newFilter((ele, index, arr) => ele % 2 == 0))
 
 
- let inputMap=[3,5,6,7]
+let inputMap = [3, 5, 6, 7]
 // //let outputMap=[4,6,7,8]
 // //console.log(inputMap.map((ele)=>++ele))
 
@@ -56,14 +56,52 @@ console.log(newoddarr2)
 
 
 
-let numarray=['lavi','suman','chhavi','divya']
+let numarray = ['lavi', 'suman', 'chhavi', 'divya']
 //o/p- lavi suman chhavi divya
 //numarray.forEach((ele,i,arr)=>console.log(ele))
 
 //polyfill foreach
-Array.prototype.newforEach=function (fn){
-    for(let i=0;i<this.length;i++){
-        fn(this[i],i,this)
+Array.prototype.newforEach = function (fn) {
+    for (let i = 0; i < this.length; i++) {
+        fn(this[i], i, this)
     }
 }
-numarray.newforEach((ele,i,arr)=>console.log(ele))
+numarray.newforEach((ele, i, arr) => console.log(ele))
+
+
+
+var obj1 = { name: 'lavi', last: 'rastogi' }
+
+function nameCaptical(city, pincode) {
+    console.log(this.name, this.last, city, pincode)
+}
+
+//polyfill of call
+
+Function.prototype.myCall = function (obj, ...args) {
+    obj._this = this;
+    obj._this(...args)
+}
+
+nameCaptical.myCall(obj1, 'lko', 201304);
+
+//polyfill for apply
+Function.prototype.myApply = function (obj, args) {
+    obj._this = this;
+    obj._this(...args)
+}
+
+nameCaptical.myApply(obj1, ['lko', 201304]);
+
+
+//polyfil for bind
+Function.prototype.myBind = function (obj, ...args) {
+    obj._this = this;
+    return function (...params) {
+        let combinedparams=[...args,...params];
+        obj._this(...combinedparams)
+    }
+}
+
+let newfunc = nameCaptical.myBind(obj1, 'lko');
+console.log(newfunc(20287))
